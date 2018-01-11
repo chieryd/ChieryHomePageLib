da82dc0c751120c325e76e3ec159c20e1eedc48c
HanDong Wang <handongwang@HanDongdeMacBook-Pro.local>
2018-01-11 20:20:53 +0800

    add resource

---
+++ library/ChieryHomePage.h
@@ -0,0 +1,12 @@
+//
+//  ChieryHomePage.h
+//  ChieryHomePage
+//
+//  Created by chiery on 2018/1/5.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface ChieryHomePage : UIViewController
+
+@end
